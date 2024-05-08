b51348a100cccf81b3c75e08c9164366c45bdecd
Long <1792144731@qq.com>
2024-05-08 15:57:25 +0800

    改错

--- 1.入门/index.js
+++ 1.入门/index.js
@@ -1,6 +1,6 @@
 //引入Threejs
 import * as THREE from 'three';
-console.log(THREE);
+// console.log(THREE);
 /**
  * 创建3D场景对象Scene
  */
@@ -12,12 +12,13 @@ const scene = new THREE.Scene();
 //创建一个长方体的几何对象
 const geometry = new THREE.BoxGeometry(50, 50, 50);
 //材质对象
-const material = new THREE.MeshLambertMaterial({
-    color: 0x0000ff,//颜色
+const material = new THREE.MeshBasicMaterial({
+    //绿色
+    color: 0x00ff00
 });
 const mesh = new THREE.Mesh(geometry, material);//网格模型对象
 //设置网格模型位置
-mesh.position.set(0, 10, 0);
+mesh.position.set(0, 0, 0);
 //添加到场景中
 scene.add(mesh);
 
@@ -31,9 +32,9 @@ const height = 500;//窗口高度
  * 透视相机设置
  */
 //创建相机对象
-const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);//透视相机
-camera.position.set(292, 223, 185);//相机位置
-camera.lookAt(0, 0, 0);//设置相机方向(指向的场景对象)
+const camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 2000);//透视相机
+camera.position.set(50, 26, 95);//相机位置
+camera.lookAt(mesh.position);//设置相机方向(指向的场景对象)
 
 /**
  * 渲染器设置
